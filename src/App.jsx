63905175ca3b8b91c84e93960f2635143aee0e93
Rexdown <56792518+Rexdown@users.noreply.github.com>
2022-04-30 19:52:03 +0400

    [fe-002]: Created main page layout and Film swiper on next section of the page. (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import MainPage from './components/mainPage/MainPage';
+
+// export function AddLibrary(urlOfTheLibrary) {
+//   const script = document.createElement('script');
+//   script.src = urlOfTheLibrary;
+//   script.async = true;
+//   document.body.appendChild(script);
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainPage />
     </div>
   );
 }
